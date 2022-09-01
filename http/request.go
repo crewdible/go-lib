020e83d4ef71149246fdd85983a309350587ac11
Muhammad Hasbi Ashshiddieqy <hasbi@crew.id>
2022-09-01 09:38:27 +0700

    add new request type

--- http/request.go
+++ http/request.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -85,4 +87,34 @@ func RequestWithoutResponse(method, url string, header map[string]string, body i
 	_, _ = client.Do(req)
 
 	return nil
+}
+
+func RequestFormUrlEncoded(method, urlPath string, header map[string]string, body map[string]string, data interface{}) error {
+	var client = &http.Client{}
+
+	urlBody := url.Values{}
+	for k, v := range body {
+		urlBody.Set(k, v)
+	}
+
+	req, _ := http.NewRequest(method, urlPath, strings.NewReader(urlBody.Encode()))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
