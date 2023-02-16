c6eaba73dce1607d7a30c23073b923317e624f44
Muhammad Afif <afif@crew.id>
2023-02-16 13:50:20 +0700

    feat:implement cross repository transaction

---
+++ db/transaction.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"context"
+
+	"github.com/crewdible/go-lib/stringlib"
+)
+
+type (
+	contextTranasction struct {
+		trxs []Transaction
+	}
+
+	Transaction interface {
+		Begin(ctx context.Context) error
+		Commit(ctx context.Context) error
+		Rollback(ctx context.Context) error
+	}
+
+	ContextTransaction interface {
+		Begin(ctx context.Context) (context.Context, error)
+		Commit(ctx context.Context) (context.Context, error)
+		Rollback(ctx context.Context) (context.Context, error)
+	}
+)
+
+func NewContextTransaction(trxs ...Transaction) ContextTransaction {
+	return &contextTranasction{trxs: trxs}
+}
+
+func (trx *contextTranasction) Begin(ctx context.Context) (context.Context, error) {
+	ctx = context.WithValue(ctx, "trx_id", stringlib.GenerateRandString(6))
+	for _, da := range trx.trxs {
+		err := da.Begin(ctx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+	return ctx, nil
+}
+
+func (trx *contextTranasction) Commit(ctx context.Context) (context.Context, error) {
+	for _, da := range trx.trxs {
+		err := da.Commit(ctx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+
+	ctx = context.WithValue(ctx, "trx_id", nil)
+	return ctx, nil
+}
+
+func (trx *contextTranasction) Rollback(ctx context.Context) (context.Context, error) {
+	for _, da := range trx.trxs {
+		err := da.Rollback(ctx)
+		if err != nil {
+			return ctx, err
+		}
+	}
+	ctx = context.WithValue(ctx, "trx_id", nil)
+	return ctx, nil
+}
